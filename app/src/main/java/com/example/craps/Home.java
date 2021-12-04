7c74fcc106825e4533c5e34cb06c594360df3108
bryanshulkin <bryanshulkin>
2021-12-03 21:02:13 -0500

    Initial commit

---
+++ app/src/main/java/com/example/craps/Home.java
@@ -0,0 +1,150 @@
+package com.example.craps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+import java.text.BreakIterator;
+import java.util.Arrays;
+
+public class Home extends AppCompatActivity {
+
+    public Button button;
+
+    double scaleFactor = 1;
+
+    public Spinner spinner;
+
+    EditText editText;
+
+    TextView avail;
+
+    private AdView mAdView;
+
+    //private String currency;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        button = (Button) findViewById(R.id.start);
+        editText = findViewById(R.id.editText);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.currency, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener {
+
+
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                // An item was selected. You can retrieve the selected item using
+                // parent.getItemAtPosition(pos)
+            }
+
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Another interface callback
+            }
+        }
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+            }
+
+            @Override
+            public void onAdFailedToLoad(LoadAdError adError) {
+                // Code to be executed when an ad request fails.
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+            }
+
+            @Override
+            public void onAdClicked() {
+                // Code to be executed when the user clicks on an ad.
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when the user is about to return
+                // to the app after tapping on an ad.
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editText = (EditText) findViewById(R.id.editText);
+                /*  int value = Integer.parseInt(editText.getText().toString());*/
+
+                String et = editText.getText().toString();
+                Intent intent = new Intent(Home.this, MainActivity.class);
+                String[] currency = getResources().getStringArray(R.array.currency);
+                double newDouble = Double.parseDouble(et);
+                for (String x : currency) {
+                    if (Arrays.asList(currency).contains("USD")) {
+                        newDouble = newDouble * 1;
+                    } else if (x == currency[0]) {
+                        newDouble = newDouble * 1.32;
+                    } else if (x == currency[0]) {
+                        newDouble = newDouble * 1.13;
+                    } else if (x == currency[0]) {
+                        newDouble = newDouble * 0.047;
+                    } else if (x == currency[0]) {
+                        newDouble = newDouble * 0.089;
+                    }
+                }
+                String finalDouble = String.valueOf(newDouble);
+
+                intent.putExtra("et", finalDouble);
+
+
+                startActivity(intent);
+
+
+                //String value = button.getText().toString();
+
+            }
+        });
+
+
+
+
+    }
+
+}
